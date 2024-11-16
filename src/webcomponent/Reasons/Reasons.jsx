2e4b824ad19402310083d0d1237e7f29aaea7ed2
kiruba04 <skirubakaran40@gmail.com>
2024-11-16 12:21:29 +0530

    bugs

--- src/webcomponent/Reasons/Reasons.jsx
+++ src/webcomponent/Reasons/Reasons.jsx
@@ -3,10 +3,10 @@ import './Reasons.css'
 const Reasons =() =>{
     return (
         <>
-         <div className="container">
+         <div className="container mt-4">
             <div>Reasons why your business needs a website</div>
             <div className="row">
-                
+                <div className='col-lg-'></div>
             </div>
         </div>
         </>
